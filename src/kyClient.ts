cac97ca02dc0e6d075eeca9e86173455160a291b
rpPanda <ruddhi@workduck.io>
2023-02-01 23:23:44 +0530

    Return true value on getting from cache

--- src/kyClient.ts
+++ src/kyClient.ts
@@ -45,7 +45,7 @@ class KYClient {
       key in this._urlHash &&
       Date.now() - this._urlHash[key] < (cacheConfig.expiry ?? DEFAULT_CACHE_EXPIRY)
     ) {
-      return
+      return this._urlHash[key]
     } else {
       const item = await this._client.get(url, options)
       if (cacheConfig?.enabled) {
